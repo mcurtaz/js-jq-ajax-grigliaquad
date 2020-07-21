16a734e6a5d0f8ecba7c91e36b5c306fe5bce908
Michele Curtaz <mcurtaz@gmail.com>
2020-07-21 14:16:01 +0200

    added ajax

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(init);
 
 function init() {
-  console.log("hello world");
+  addBtnListener();
+}
+
+function addBtnListener() {
+  var target = $("#btn");
+  target.click(creaQuadratino);
+}
+
+function creaQuadratino() {
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/random/int",
+    method: "GET",
+    success: function (data, state) {
+      console.log(data);
+      console.log(state);
+    },
+    error: function (request, state, errors) {
+      console.log(request);
+      console.log(state);
+      console.log(errors);
+    }
+  });
 }
